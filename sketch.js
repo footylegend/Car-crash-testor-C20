8068281f1a31f1f636a2efd511e973e7a532a3e2
footylegend <66203511+footylegend@users.noreply.github.com>
2020-06-18 15:45:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var car,wall;
+var speed,weight,deformation;
+
+function setup() {
+  createCanvas(1600,400);
+  speed=Math.round(random(55,90));
+  weight=Math.round(random(400,1500));
+ 
+  car=createSprite(50,200,50,20);
+  wall=createSprite(1200,200,40,200);
+
+  wall.shapeColor="red";
+
+  car.velocityX=speed;
+
+  deformation=(0.5*weight*car.velocityX*car.velocityX)/22500;
+
+ 
+}
+
+function draw() {
+  
+  background("black");
+
+   
+
+ if(isTouching()){
+  
+   car.collide(wall);
+   if(deformation<100){
+      car.shapeColor="green";
+   }
+   if(deformation>100 && deformation<180){
+      car.shapeColor="yellow";
+   }
+   if(deformation>180){
+      car.shapeColor="red";
+   }
+ }
+ else{
+      car.shapecolor="blue";
+ }
+ 
+
+  drawSprites();
+ 
+}
+function isTouching(){
+  if(wall.x - car.x < car.width/2 + wall.width/2){
+    return true;
+  }
+  else {
+    return false;
+   }
+}
